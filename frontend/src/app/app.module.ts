9d89735a1f10279820985c8caf50b42b4de8d48f
flaviusp23 <flavius.paltin@yahoo.com>
2024-07-04 21:48:27 +0300

    like unlike a post

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlaygroundModule } from './playground/playground.module';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -24,7 +23,6 @@ import { CreatepostComponent } from './createpost/createpost.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PlaygroundModule,
     FormsModule
   ],
   providers: [provideHttpClient()],
--- frontend/src/app/post/post.component.ts
+++ frontend/src/app/post/post.component.ts
@@ -1,16 +1,42 @@
-import { Component , Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from '../app.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrls: ['./post.component.scss']
 })
 export class PostComponent {
-  username = 'Andrei';
+  username = localStorage.getItem('username') || '';
   userAvatarUrl =
     'https://aui.atlassian.com/aui/9.1/docs/images/avatar-person.svg';
 
-  constructor() {}
+  constructor(
+    private router: Router,
+    private appService: AppService
+  ) {}
 
   @Input() postInput: any;
+
+  onLike() {
+    this.appService
+      .likePost(this.postInput.id, this.username)
+      .pipe(first())
+      .subscribe({
+        next: (response) => {
+          this.postInput.likes = response.likes;
+          this.postInput.likeCount = response.likeCount;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+  }
+
+  redirectToPost(postId: string) {
+    this.router.navigate(['/posts', postId]);
+    localStorage.setItem('postId', postId);
+  }
 }
