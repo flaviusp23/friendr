4e9230d8702fa15940f023155354539ad20acdaa
flaviusp23 <flavius.paltin@yahoo.com>
2024-07-10 22:31:29 +0300

    poate merge

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(port, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`Example app listening on port ${port}`);
     });
   })
